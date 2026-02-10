37c8b6fbe84778707fb53df38df49211a98038ab
TheerthaPraveen9817 <theerthapraveen026@gmail.com>
2026-02-10 10:23:47 +0530

    latest changes

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 const Add = () => {
 
-  const url="http://localhost:4000";
+  const url="https://food-delivery-mern-cpcg.onrender.com";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
