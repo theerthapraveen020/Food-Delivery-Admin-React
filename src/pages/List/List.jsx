69418c48cd762480ec1721f1dfbfa7e71acb3a5b
theerthapraveen020 <theerthapraveen020@gmail.com>
2026-02-10 11:19:35 +0530

    fixed bug

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import {toast} from "react-toastify"
 const List = () => {
 
-  const url ="https://food-delivery-mern-cpcg.onrender.com/"
+  const url ="https://food-delivery-mern-cpcg.onrender.com"
   const [list,setList]=useState([]);
   const fetchList =async()=>{
     const response =await axios.get(`${url}/api/food/list`);
